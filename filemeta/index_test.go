51ce249f119833e065e98c5e6044a55366a485d2
Pascal Dennerly <dnnrly@gmail.com>
2019-10-14 23:15:00 +0100

    Add file content hash to index

--- filemeta/index_test.go
+++ filemeta/index_test.go
@@ -10,18 +10,24 @@ func TestNewIndex(t *testing.T) {
 	buf := `files:
   a/b/c:
     key: root/a/b/c
+    hash: 123
   d/e:
     key: root/d/e
+    hash: 456
   f:
     key: root/f
+    hash: 789
 `
 
 	index, err := NewIndex(buf)
 	assert.NoError(t, err)
 	assert.Equal(t, 3, len(index.Files))
 	assert.Equal(t, "root/a/b/c", index.Files["a/b/c"].Key)
+	assert.Equal(t, "123", index.Files["a/b/c"].Hash)
 	assert.Equal(t, "root/d/e", index.Files["d/e"].Key)
+	assert.Equal(t, "456", index.Files["d/e"].Hash)
 	assert.Equal(t, "root/f", index.Files["f"].Key)
+	assert.Equal(t, "789", index.Files["f"].Hash)
 }
 
 func TestNewIndex_ParseError(t *testing.T) {
@@ -38,7 +44,8 @@ func TestEncode(t *testing.T) {
 	i := Index{
 		Files: map[string]Sourcefile{
 			"1/2/3": Sourcefile{
-				Key: "a/b/c",
+				Key:  "a/b/c",
+				Hash: "123",
 			},
 		},
 	}
@@ -47,24 +54,25 @@ func TestEncode(t *testing.T) {
 	assert.Equal(t, `files:
     1/2/3:
         key: a/b/c
+        hash: "123"
 `, out)
 }
 
 func TestIndexDifference(t *testing.T) {
 	local := &Index{
 		Files: map[string]Sourcefile{
-			"1": Sourcefile{Key: "a"},
-			"2": Sourcefile{Key: "b"},
-			"3": Sourcefile{Key: "c"},
-			"4": Sourcefile{Key: "d"},
+			"1": Sourcefile{Key: "a", Hash: "321"},
+			"2": Sourcefile{Key: "b", Hash: "123"},
+			"3": Sourcefile{Key: "c", Hash: "123"},
+			"4": Sourcefile{Key: "d", Hash: "123"},
 		},
 	}
 	remote := &Index{
 		Files: map[string]Sourcefile{
-			"1": Sourcefile{Key: "a"},
-			"2": Sourcefile{Key: "b"},
-			"4": Sourcefile{Key: "d"},
-			"5": Sourcefile{Key: "e"},
+			"1": Sourcefile{Key: "a", Hash: "123"},
+			"2": Sourcefile{Key: "b", Hash: "123"},
+			"4": Sourcefile{Key: "d", Hash: "123"},
+			"5": Sourcefile{Key: "e", Hash: "123"},
 		},
 	}
 
